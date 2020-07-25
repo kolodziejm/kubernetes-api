3844c513beb3c3b213ab42eaa10f40d2927155ff
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2020-07-25 13:22:21 +0200

    Added cors

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json('Some example response');
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
